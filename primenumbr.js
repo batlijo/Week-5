934a65d415bfc80448781a7f7df79dc5db2b35b8
Batlijo <batlijoselevitz@gmail.com>
2017-10-17 09:00:33 -0700

    week5notes

---
+++ primenumbr.js
@@ -0,0 +1,28 @@
+/*	Write a function that checks to see if a number is a prime number or not.
+	Have it return true if it is, or false if it's not.
+	for each number 1 through 1000 do these things
+	if number is divisible by 1 and itself only, it's true
+	if number is not divisible by and itself only, it's false
+	how do you figure out to see if a number is divisible? */
+
+
+var i = num
+// for loop 1 through 1000
+for (i = 2; i < num; i+=) {
+	console.log(loop);
+}
+
+function isPrime(num) {
+	//returns true if prime
+	// returns false if not prime
+	5
+	2,3,4,
+}
+
+for each num 1 through 1000...
+	prime = isPrime(num)
+if prime then print num 
+
+if(num % i === 0){
+	returns false;
+}
